8f6a1cc3ef56ab603795594ad518807b80e475aa
MEGAPETER <pkoutras@terpmail.umd.edu>
2021-06-08 18:03:26 -0700

    Just a commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import adam from './Adam.jpeg';
 import {FormEvent, Component, ChangeEvent} from 'react';
 import './App.css';
 
@@ -8,7 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Upload your image here: </p>
+        <img src="Adam.jpeg"></img>
         <ImageUpload />
+        <PostPage />
       </header>
     </div>
   );
@@ -44,12 +46,50 @@ class ImageUpload extends Component<{}, ImageUploadState> {
     return (
       <div>
         <input type="file" accept="image/*" onChange={(event) => this.handlePictureUpload(event)} multiple = {false} />
-        <img src = {this.state.image}
-        ></img>
+        <img src = {this.state.image}></img>
       </div>
     );
   }
+}
 
+interface Post {
+  content: String
+}
+
+interface PostPageState {
+  posts: Post[];
+}
+ 
+class PostPage extends React.Component<{}, PostPageState> {
+  constructor({}) {
+    super({});
+    this.state = {
+      posts: [{content: "existing post"}],
+    };
+  }
+  submitPost(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const submittedContent = event.currentTarget.contentField.value;
+    this.setState({
+      posts: [...this.state.posts, {content: event.currentTarget.contentField.value}],
+    });
+    event.currentTarget.contentField.value = '';
+  }
+ 
+  render() {
+    return (
+      <div className="App">
+        <h1>Sample Post page:</h1>
+        
+        {this.state.posts.map(post => <p>{post.content}</p>)}
+ 
+        <form onSubmit={e => this.submitPost(e)}>
+          <input name="contentField" type="text" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
